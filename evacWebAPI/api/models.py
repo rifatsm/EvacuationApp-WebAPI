b943680bc526b8292e4c8990256d54b6d614cada
rifatsm <rifatsm@vt.edu>
2019-02-17 00:33:33 -0500

    # Location model created

--- evacWebAPI/api/models.py
+++ evacWebAPI/api/models.py
@@ -3,14 +3,12 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-	"""docstring for ClassName"""
 	name = models.CharField(max_length=50)
 	# identifier = models.IntegerField()
 	username = models.CharField(max_length=20, unique=True, blank=True, null=True)
 	password = models.CharField(max_length=50, blank=True, null=True)
 	email = models.EmailField(max_length=50, blank=True, null=True)
 	group = models.IntegerField()
-	created_at = models.DateTimeField(auto_now_add=True)
 	role_admin = models.BooleanField(default=0)
 	current_state = models.BooleanField(default=0)
 
@@ -31,3 +29,12 @@ class User(models.Model):
 		return (self.username == username and self.password == password)
 	def login_authenticate_email(self, email, password):
 		return (self.email == email and self.password == password)
+
+class Location(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	longitude = models.DecimalField(max_digits=9, decimal_places=6)
+	latitude = models.DecimalField(max_digits=9, decimal_places=6)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return 'user: %d longitude: %s latitude: %s created_at: %s' % (self.user, self.longitude, self.latitude, self.created_at)
