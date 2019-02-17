3ef5779fbf339cecb4904ee61b1a3eeae5cb61e8
rifatsm <rifatsm@vt.edu>
2019-02-16 21:10:20 -0500

    # Admin Role Implemented

--- evacWebAPI/api/models.py
+++ evacWebAPI/api/models.py
@@ -9,9 +9,10 @@ class User(models.Model):
 	current_state = models.BooleanField(default=0)
 	group = models.IntegerField()
 	created_at = models.DateTimeField(auto_now_add=True)
+	role_admin = models.BooleanField(default=0)
 
 	def __str__(self):
-		return '%d %d' % (self.identifier, self.group)
+		return '%d %d %s' % (self.identifier, self.group, self.role_admin)
 	def current_panic_state(self):
 		return bool(self.current_state)
 	def get_identifier(self):
@@ -21,3 +22,5 @@ class User(models.Model):
 	def change_state(self):
 		self.current_state = not self.current_state
 		return self.current_state
+	def get_role(self):
+		return self.role_admin
