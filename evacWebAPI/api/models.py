2224b15676216b42742619870475d712786bb8c9
rifatsm <rifatsm@vt.edu>
2019-02-16 20:29:45 -0500

    # User Model created

--- evacWebAPI/api/models.py
+++ evacWebAPI/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	"""docstring for ClassName"""
+	name = models.CharField(max_length=50)
+	identifier = models.IntegerField()
+	current_state = models.BooleanField(default=0)
+	group = models.IntegerField()
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return '%d %d' % (self.identifier, self.group)
+	def current_panic_state(self):
+		return bool(self.current_state)
+	def get_identifier(self):
+		return self.identifier
+	def get_group(self):
+		return self.group
