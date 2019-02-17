74f539ecee2dc60bc948ea42ff8a95f1993f78d0
rifatsm <rifatsm@vt.edu>
2019-02-17 03:24:11 -0500

    # Location API modified

--- evacWebAPI/api/resources.py
+++ evacWebAPI/api/resources.py
@@ -1,4 +1,5 @@
 from tastypie.resources import ModelResource
+from tastypie import fields
 from api.models import User, Location
 from tastypie.authorization import Authorization
 
@@ -6,12 +7,22 @@ class UserResource(ModelResource):
 	class Meta:
 		queryset = User.objects.all()
 		resource_name = 'user'
+		fields = ['id', 'username', 'group', 'role_admin', 'current_state']
+		filtering = {
+			'username' : ['exact'],
+			'group'	:	['exact']
+		}
 		authorization = Authorization()
 
 class LocationResource(ModelResource):
+	user = fields.ForeignKey(UserResource, 'user')
 	class Meta:
 		queryset = Location.objects.all()
 		resource_name = 'location'
 		authorization = Authorization()
+
+# class MarkSafe(ModelResource):
+# 	class Meta: 
+# 		queryset = User.objects.all()
 
 
